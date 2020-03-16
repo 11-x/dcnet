bc765e49c0582791e21c4830417b3e9348ce72c2
11-x <chpasswd@mail.ru>
2020-03-17 00:00:12 +0700

    minor work

--- htdocs/main.js
+++ htdocs/main.js
@@ -1,10 +1,11 @@
 function main()
 {
 	if (location.pathname=='/') {
-		if (!auth.is_logged_in()) {
+		if (!auth.is_logged_in) {
 			page.go('gate');
 		}
 	} else {
+		// This page is normally invoked as /page_name
 		page.go(location.pathname.slice(1));
 	}
 }
